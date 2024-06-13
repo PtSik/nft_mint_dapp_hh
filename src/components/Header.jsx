d5c1196912639a67eb946636a91138f55a484360
Patryk <patrykgniewek9@gmail.com>
2024-06-13 13:54:25 +0200

    init

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <nav className="w-4/5 fllex justify-between 
+    md:justify-center items-center py-4 mx-auto"
+    >
+      <div className="flex flex-row justify-start items-center md:flex-[0.5] flex-initial">
+        <img src="" alt="" />
+      </div>
+
+    </nav>
+  )
+}
+
+export default Header
